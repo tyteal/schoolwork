1e8b1137abef5d17c84062273de30aba45128f7a
tyteal <gamerteal19@gmail.com>
2025-03-10 15:59:35 -0600

    Update poop.lua

--- poop.lua
+++ poop.lua
@@ -89,7 +89,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
     ["Fields"] = {
         ['AimAssistField'] = { ['Size'] = (500) },
         ['SilentAimField'] = { ['Size'] = (100) },
-        ['SilentAimBoxField'] = { ['Visible'] = (false), ['Sync'] = (false), ['Width'] = (2.5), ['Height'] = (4.5) }
+        ['SilentAimBoxField'] = { ['Visible'] = (false), ['Sync'] = (false), ['Width'] = (4.5), ['Height'] = (6.5) }
     },
 }
 
