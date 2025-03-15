afa0181dc6ecdde96e104f993327503d18810a0a
tyteal <gamerteal19@gmail.com>
2025-03-15 10:44:55 -0600

    Update tll.lua

--- tll.lua
+++ tll.lua
@@ -48,7 +48,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
     
         ['Interval'] = { ['Enabled'] = (false), ['Weapon'] = { ['[Double-Barrel SG]'] = (1), ['[TacticalShotgun]'] = (1), ['[Revolver]'] = (1) } }, --[[ In MS ]]--
         --[[ Cursor mode doesn't work on offbrands due to hitboxes, use this Magnitude instead ]]
-        ['Magnitude'] = { ['Weapon'] = { ['[Double-Barrel SG]'] = { X = (30), Y = (44) }, ['[TacticalShotgun]'] = { X = (24), Y = (44) }, ['[Revolver]'] = { X = (28), Y = (50) } } } --[[ Keep these lower for legit cheating, higher for rage ]]
+        ['Magnitude'] = { ['Weapon'] = { ['[Double-Barrel SG]'] = { X = (30), Y = (44) }, ['[TacticalShotgun]'] = { X = (24), Y = (44) }, ['[Revolver]'] = { X = (26), Y = (48) } } } --[[ Keep these lower for legit cheating, higher for rage ]]
     },
 
     ["Physics"] = {
